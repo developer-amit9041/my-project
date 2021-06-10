06b75d94646f967ed8a1e4628805a05095d8a512
panjlaamit01 <ak.amit9041@gmail.com>
2021-06-11 01:57:15 +0530

    my first website

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database_name = "demo";
+$conn = mysqli_connect($host, $username, $password, $database_name) or die("Connection failed: " . mysqli_error());
+?>
